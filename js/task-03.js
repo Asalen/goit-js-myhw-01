40eed4f0ae7312ca4c156a99c6118cb21d4fc794
ANDRII ALENIN <mkalenin@gmail.com>
2020-12-29 22:01:38 +0200

    Выполняет 10 задач

---
+++ js/task-03.js
@@ -0,0 +1,11 @@
+let topSpeed = 160;
+let distance = 617.54;
+let login = 'mango935';
+let isOnline = true;
+let isAdmin = false;
+
+console.log(topSpeed);
+console.log(distance);
+console.log(login);
+console.log(isOnline);
+console.log(isAdmin);
